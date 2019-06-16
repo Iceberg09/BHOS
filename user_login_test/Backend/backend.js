398560a8ffb94047eb3a5a15ff61e53407faf483
MazenAbusharkh <abusha1@stolaf.edu>
2019-06-16 18:42:22 -0500

    fixing the backend to be able to receive both POST and GET requests and handle them with the SQL database correctly

--- user_login_test/Backend/backend.js
+++ user_login_test/Backend/backend.js
@@ -1,11 +1,78 @@
 // load our backend server using express
-const express = require('express')
-const backend = express()
-const morgan = require('morgan')
-const mysql = require('mysql')
+const express = require('express');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-backend.use(morgan('combined'))
+const backend = express();
 
+// Using the express body-parser to be able to read values as sent from the frontend's fretch (POST API) function
+backend.use(bodyParser.json());
+
+backend.use(morgan('combined'));
+
+// If the backend receives as a POST API request
+backend.post('/user', (req, res) => {
+	console.log("Posting a new user. User's new id = ")
+	
+	// connect to the project's local database (BHOS_login_data) under user "root", which has a password "password"
+	const connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'password',
+		database: 'BHOS_login_data'
+	})
+
+	// Assign the details sent from the frontend to different variables to be used later
+	const myUsers = 'users'
+	const userName = req.body.full_name
+	const userEmail = req.body.user_email
+	const userPassword = req.body.user_password
+	console.log(userName);
+
+	// the INSERT SQL command -- the question marks allow us to add another string/variable in their place when sending the command to the SQL database
+	const insertString = "INSERT INTO ?? (full_name, user_email, user_password) VALUES (?,?,?);"
+
+	// run the query (insertString) on the variable 'connection', which is a mysql connection (as introduced by the mysql node plugin)
+	// replace the question mark in the query with the different details sent by the frontend by placing it after it in between [] within
+	//  the query function (a function introduced by the mysql node plugin) the last parameter in the 'query' function is an arrow function
+	// with three parameters, errors, rows (indecies), and fields (cells) -- we're only using 'err' for now.
+	connection.query(insertString, [myUsers, userName, userEmail, userPassword], (err, rows, fields) => {
+		// catch errors
+		if (err) {
+			console.log("Failed to insert the user: " + err)
+			res.sendStatus(500)
+			throw err
+			// OR // return
+		}
+		console.log("I think we inserted the user successfully")
+	})
+
+	// another query, but this time for confirming that the inserted user credentials actually got inserted into the database (by querying it)
+	const queryString = "SELECT * FROM ?? WHERE id=LAST_INSERT_ID();"
+		
+	connection.query(queryString, [myUsers], (err, rows) => {
+		if (err) {
+			console.log("Failed to query for last inserted user: " + err)
+			res.sendStatus(500)
+			throw err
+			// OR // return
+		}
+		console.log("I think we fetched the last iserted user successfully")
+
+		// just changing the way the outputted JSON looks -- only outputting the id, full name, and email (and not the password).
+		const users = rows.map((row) => {
+			return {userId: row.id, fullName: row.full_name, userEmail: row.user_email}
+		})
+
+		// send the outputted JSON in the REST API response as a JSON object
+		res.json(users)
+	})	
+
+	// res.end()
+})
+
+// If the backend receives as a GET API request
 backend.get('/user/:id', (req, res) => {
 	console.log("Fetching user with id: " + req.params.id)
 	
@@ -18,51 +85,40 @@ backend.get('/user/:id', (req, res) => {
 	})
 
 	// Set the id inputted in the client's request to a variable called 'userId' for cleanliness
+	const myUsers = 'users'
 	const userId = req.params.id
 
-	// Create a sting with a question mark for the query -- the question mark allows me to add another string/variable in its place later on
-	// const queryString = "SELECT * FROM !!!!!! users WHERE id = ?" // INTENTIONALLY WRONG QUERY
-	const queryString = "SELECT * FROM users WHERE id = ?"
+	const queryString = "SELECT * FROM ?? WHERE id = ?"
 
-	// run the query on the variable 'connection', which is a mysql connection (as introduced by the mysql node plugin)
-	// replace the question mark in queryString with userId by placing it after it in between [] within the query function (a function introduced by the mysql node plugin)
-	// the last parameter in the 'query' function is an arrow function with three parameters, errors, rows (indecies), and fields (cells)
-	connection.query(queryString, [userId], (err, rows, fields) => {
-		// catch errors
+	connection.query(queryString, [myUsers,userId], (err, rows) => {
 		if (err) {
 			console.log("Failed to query for users: " + err)
 			res.sendStatus(500)
 			throw err
-			// OR // return
 		}
 		console.log("I think we fetched users successfully")
-		// output the index(s) of the id(s) through a response as a JSON (object)
 
-		// just changing the way the outputted JSON looks -- only outputting first names and calling them firstName (not first_name) etc.
 		const users = rows.map((row) => {
-			return {firstName: row.first_name, lastName: row.last_name}
+			return {fullName: row.full_name, userEmail: row.user_email}
 		})
 
 		res.json(users)
-	})
-	
-	// res.end()
-})
-
-backend.get("/", (req, res) => {
-	console.log("Responding to root route")
-	res.send("Hello from ROOOOT!")
+	})	
 })
 
 backend.get("/users", (req, res) => {
-	var user1 = {firstName: "Stephen", lastName: "Curry"}
-	const user2 = {firstName: "Kevin", lastName: "Durant"}
+	var user1 = {firstName: "Mazen", lastName: "Abu"}
+	const user2 = {firstName: "Josef", lastName: "Mendez"}
 	res.json([user1, user2])
 })
 
-backend.listen()
-
-// localhost:3003 
+// run the server on localhost:3003 and have it start listening for REST API requests 
 backend.listen(3003, () => {
 	console.log("Server is up and listening on 3003!!!");
+})
+
+// catching any 404s
+backend.get("*", (req, res) => {
+	console.log("Responding to root route")
+	res.send(`Where are you going!!!???`)
 })
