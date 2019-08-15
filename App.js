866f97588ffb08a6c488e8fdfb63ccce28b4a82c
Jacob Dahlberg <jdahlberg@peoplenetonline.com>
2019-08-15 14:43:27 -0500

    Added basic duty status change page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Dimensions, SafeAreaView, TextInput, Button, Alert, Text } from 'react-native';
+import { AppRegistry, View, Dimensions, SafeAreaView, TextInput, Button, Alert, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Image from 'react-native-scalable-image';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
@@ -42,6 +42,78 @@ class LoginScreen extends Component {
 
 };
 
+class ChangeDutyStatusScreen extends Component {
+
+  onDutyButtonPress() {
+    alert("Your duty status is now set to on duty");
+  };
+
+  offDutyButtonPress() {
+    alert("Your duty status is now set to off duty");
+  };
+
+  drivingButtonPress() {
+    alert("Your duty status is now set to driving");
+  };
+
+
+
+  render() {
+    return (
+
+      <SafeAreaView style={{ backgroundColor: '#005a9c' }}>
+
+        <Text style={{ textAlign: 'center', fontSize: 24, fontWeight: 'bold', paddingTop: 60 }}>Duty Status Change Screen</Text>
+
+        <View style={styles.container}>
+
+          <TouchableOpacity onPress={this.onDutyButtonPress}>
+
+            <View
+              style={styles.button}
+              backgroundColor='#089946'
+            >
+
+              <Text style={styles.buttonText}>On Duty</Text>
+
+            </View>
+
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.offDutyButtonPress}>
+
+            <View
+              style={styles.button}
+              backgroundColor='#CB1B01'
+            >
+
+              <Text style={styles.buttonText}>Off Duty</Text>
+
+            </View>
+
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.drivingButtonPress}>
+
+            <View
+              style={styles.button}
+              backgroundColor='#006AFF'
+            >
+
+              <Text style={styles.buttonText}>Driving</Text>
+
+            </View>
+
+          </TouchableOpacity>
+
+        </View>
+
+      </SafeAreaView>
+
+    )
+  }
+}
+
 class HomeScreen extends Component {
 
   render() {
@@ -50,11 +122,22 @@ class HomeScreen extends Component {
 
       <SafeAreaView>
 
-      <View>
+        <View>
 
-        <Text>You are now logged in!</Text>
+          <Text>You are now logged in!</Text>
 
-      </View>
+        </View>
+
+        <View style={styles.buttonContainer}>
+
+          <Button
+            title='Change Duty Status'
+            onPress={() => this.props.navigation.navigate('ChangeDutyStatus')}
+          />
+
+        </View>
+
+
 
       </SafeAreaView>
 
@@ -66,12 +149,37 @@ class HomeScreen extends Component {
 const AppNavigator = createStackNavigator(
   {
     Login: LoginScreen,
-    Home: HomeScreen
+    Home: HomeScreen,
+    ChangeDutyStatus: ChangeDutyStatusScreen
   },
   {
     initialRouteName: 'Login'
   }
 );
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 150,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white'
+  },
+  changeScreenButton: {
+    justifyContent: 'center'
+  }
+});
 
 export default createAppContainer(AppNavigator);
 
